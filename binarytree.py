8819c280a00ef0f47d387706c03e052b5cf5ef6d
WC <63750347+wchen6544@users.noreply.github.com>
2022-07-04 23:55:34 -0400

    Add files via upload

--- binarytree.py
+++ binarytree.py
@@ -57,9 +57,9 @@ class Solution:
 
     '''
     
-    - first transverse left, 
+    - first traverse left, 
     - second appends the root value to list
-    - lastly transverse right 
+    - lastly traverse right 
 
     '''
 
@@ -82,8 +82,8 @@ class Solution:
     '''
 
     - first appends the root value to the list
-    - second transverse left
-    - lastly transverse right
+    - second traverse left
+    - lastly traverse right
 
     '''
 
@@ -106,8 +106,8 @@ class Solution:
 
     '''
     
-    - first transverse left
-    - second transverse right
+    - first traverse left
+    - second traverse right
     - lastly append the root value to the list
 
 
@@ -164,9 +164,9 @@ class Solution:
 
         '''
 
-        - first transverse left
+        - first traverse left
         - second set current right Node to the root value, then set current Node to equal current's right Node.
-        - lasly transverse right
+        - lasly traverse right
 
         '''
 
@@ -215,14 +215,205 @@ class Solution:
 
         '''
 
-        - first transverse left
+        - first traverse left
         - then set up hashmap and add occurances to the map
-        - next transverse right
+        - next traverse right
 
         - loop through the hashmap and check for max occurances (map.values)
         - return a list of values that appear the most number of times
 
         '''
+
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        self.res = 0
+        
+        def loop(root):
+            
+            if not root: return
+                
+            if root.val >= low and root.val <= high: self.res += root.val
+            
+            loop(root.left)
+            loop(root.right)
+            
+        loop(root)
+        return self.res
+
+        '''
+        - check if root value fits the low and high values
+        - tranverse left
+        - tranverse right
+
+        '''
+
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        
+        self.res = None
+        
+        def loop(root):
+            
+            if not root: return
+            
+            if root.val == target.val: self.res = root
+            
+            loop(root.left)
+            loop(root.right)
+        
+        loop(cloned)
+        return self.res
+
+        '''
+        - check if cloned root value is equal to target value
+        - tranverse left
+        - tranverse right
+
+        '''
+
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        
+        return root.left.val + root.right.val == root.val
+
+        '''
+
+        - check if left root plus right root is equal to parent root value
+
+        '''
+
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        self.bal = True
+        
+        def loop(root):
+            
+            if not root:
+                return -1
+            
+            else:
+                
+                l = loop(root.left)
+                r = loop(root.right)
+
+                print(l, r)
+                if abs(l - r) > 1: self.bal = False
+                
+                return 1 + max(l, r)
+        
+        loop(root)
+        return self.bal
+
+
+        '''
+
+        - tranverse through the tree and check if root.
+        - if not a root then return -1
+        - if root, continue the looping and return 1 + max(l, r)
+
+                (1)
+                /
+              (2)
+              /
+            (3)
+
+        - at (3), l and r should be -1 and -1
+        - at (2), l should be 0 and r should be -1
+        - at (1), l should be 1 and r should be -1 and abs(-1 - 1) is greater than 1
+
+
+        '''
+
+
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+
+        self.same = True
+        
+        def loop(p, q):
+
+            if not p and not q: return
+            
+            if (not p) or (not q) or p.val != q.val:
+                
+                self.same = False
+                
+                return
+            
+            loop(p.left, q.left)
+            loop(p.right, q.right)
+        
+        loop(p, q)
+        
+        return self.same
+
+
+        '''
+        
+        - tranverse through the trees and FIRST check if both not exist 
+        - check if one exist and one doesn't
+        - check if the values are equal
+
+        '''
+
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        self.sym = True
+
+        def loop(l, r):
+
+            if not l and not r:
+
+                return
+
+            if (not l or not r) or l.val != r.val:
+
+                self.sym = False
+
+                return
+
+
+            loop(l.left, r.right)
+            loop(l.right, r.left)
+
+
+        if root.left and root.right or not root.left and not root.right: 
+            
+            loop(root.left, root.right)
+            return self.sym
+        
+        if root.left or root.right:
+
+            return False
+            
+
+
+    '''
+
+    - first check if the tree has either (both a left and right starting pair) or (none)
+    and if these conditions are met loop through the loop with root.left and root.right
+        Example:
+            (3)       or       (3)
+            / \
+           (2)(5)
+
+    - if the previous conditions are not met, return False if there is only a (root.left),
+    but no (root.right), vise versa.
+
+    - in the loop, check if (l and r both don't exist), 
+    if this is the case, return and continue (still symmetric)
+
+    
+    - now check if one of the roots are missing (no left but right, or left but no right), and
+    check if the root values are not equal. If either of these conditions are met, set the sym
+    to FALSE and return
+
+    '''
+
+
+
+
+
 
 
 a = TreeNode(4)
@@ -233,19 +424,21 @@ a.right = TreeNode(7)
 a.right.left = TreeNode(6)
 a.right.right = TreeNode(9)
 
-b = TreeNode(5)
-b.left = TreeNode(3)
-b.right = TreeNode(6)
-b.left.left = TreeNode(2)
-b.left.right = TreeNode(4)
-b.left.left.left = TreeNode(1)
-b.right.right = TreeNode(8)
-b.right.right.left = TreeNode(7)
-b.right.right.right = TreeNode(9)
+
+p = TreeNode(1)
+p.left = TreeNode(2)
+p.left.left = TreeNode(3)
+p.left.right = TreeNode(4)
+p.right = TreeNode(2)
+p.right.right = TreeNode(3)
+p.right.left = TreeNode(4)
+
+q = TreeNode(2)
+q.left = TreeNode(3)
 
 
 s = Solution()
-print(print2D(s.increasingBST(a)))
-print(print2D(s.invertTree(a)))
-
+#print(print2D(s.increasingBST(a)))
+#print(print2D(s.invertTree(a)))
+print(s.isSymmetric(q))
 
